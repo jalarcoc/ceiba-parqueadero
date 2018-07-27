8881eb24cb95b2f06cf90e22d1bf6bd198193092
Jessica Alarcón Cardona - Ceiba Software <jessica.alarcon@ceiba.com.co>
2018-07-27 15:01:19 -0500

    committed new files respect to the main project

---
+++ src/main/java/com/example/ceibaParqueadero/CeibaParqueaderoApplication.java
@@ -0,0 +1,12 @@
+package com.example.ceibaParqueadero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CeibaParqueaderoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CeibaParqueaderoApplication.class, args);
+	}
+}
