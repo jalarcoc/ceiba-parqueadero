43fc468cd6c730588f6485e238197ca3d4face26
CEIBA\jessica.alarcon <jessica.alarcon@ceiba.com.co>
2018-08-02 07:01:53 -0500

    Nombre paquete

--- src/main/java/com/example/ceibaParqueadero/CeibaParqueaderoApplication.java
+++ src/main/java/com/example/ceibaParqueadero/CeibaParqueaderoApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages ={"dominio","persistencia.repositorio","com.example.ceibaparqueadero"})
+@ComponentScan(basePackages ={"dominio","persistencia.repositorio","com.example.ceibaParqueadero"})
 @EntityScan("persistencia.entidad")
 public class CeibaParqueaderoApplication {
 
