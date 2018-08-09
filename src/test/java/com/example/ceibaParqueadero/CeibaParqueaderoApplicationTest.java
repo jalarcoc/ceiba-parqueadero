dbbd3afd6467a1f0228543767773e31e8a654231
CEIBA\jessica.alarcon <jessica.alarcon@ceiba.com.co>
2018-08-09 10:15:41 -0500

    no message

--- src/test/java/com/example/ceibaParqueadero/CeibaParqueaderoApplicationTest.java
+++ src/test/java/com/example/ceibaParqueadero/CeibaParqueaderoApplicationTest.java
@@ -1,5 +1,6 @@
 package com.example.ceibaParqueadero;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -9,6 +10,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class CeibaParqueaderoApplicationTest {
 
+	@Before
 	@Test
 	public void contextLoads() {
 		
